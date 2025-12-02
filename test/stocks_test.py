84bab8ff93e918f94b8a76ecf6bd4991573b15f7
sth80-byte <sth80@georgetown.edu>
2025-12-02 03:44:08 -0500

    trying to make test pass in github

--- test/stocks_test.py
+++ test/stocks_test.py
@@ -4,13 +4,12 @@ from app.stocks import fetch_stocks_csv
 
 
 def test_data_fetching():
-  
+
     stocks_df = fetch_stocks_csv("GOOGL")
 
     assert isinstance(stocks_df, DataFrame)
 
-    assert "adjusted_close" in stocks_df.columns
-
     assert "timestamp" in stocks_df.columns
+    assert "adjusted_close" in stocks_df.columns
 
     assert len(stocks_df) >= 100
