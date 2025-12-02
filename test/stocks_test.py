6e8ac4b07ad6df3a4af2d048d12912493d61bbeb
sth80-byte <sth80@georgetown.edu>
2025-12-02 02:39:27 -0500

    Stocks Dashboard(#4)
    * Stocks Dashboard code
    
    * Refactor data fetching logic
    
    * Add stocks test
    
    * Update CI workflow file to use env var

---
+++ test/stocks_test.py
@@ -0,0 +1,16 @@
+from pandas import DataFrame
+
+from app.stocks import fetch_stocks_csv
+
+
+def test_data_fetching():
+  
+    stocks_df = fetch_stocks_csv("GOOGL")
+
+    assert isinstance(stocks_df, DataFrame)
+
+    assert "adjusted_close" in stocks_df.columns
+
+    assert "timestamp" in stocks_df.columns
+
+    assert len(stocks_df) >= 100
