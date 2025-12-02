db4470f8a35ac141ae283ad39766b274c61ee8c7
sth80-byte <sth80@georgetown.edu>
2025-12-01 23:20:27 -0500

    Testing (#3)
    * Refactor winner determination logic
    
    * set up tests

--- README.md
+++ README.md
@@ -10,6 +10,24 @@ Navigate to the repo using the command line. Perhaps onto the Desktop folder.
 cd ~/Desktop/local_repository/my-first-repo-fall-25
 ```
 
+Crrate a virtual environment:
+
+```sh
+conda create -n my-first-env-fall-25 python=3.11
+```
+
+Activate the virtual environment:
+
+```sh
+conda activate my-first-env-fall-25
+```
+
+Install package dependencies:
+
+```sh
+pip install -r requirements.txt
+```
+
 ## Usage
 
 Example script:
@@ -25,4 +43,13 @@ python app/rps.py
 
 # alternative "modular style" command:
 python -m app.rps
+```
+
+
+## Testing
+
+Run tests:
+
+```sh
+pytest
 ```
--- app/rps.py
+++ app/rps.py
@@ -3,33 +3,51 @@ import random
 #valid options
 valid_choices=["rock", "paper", "scissors"]
 
-#input validation loop
-while True:
-    user_choice=input("Please choose rock, paper, or scissors: ").strip().lower()
-    if user_choice in valid_choices:
-        break
+def determine_winner(u, c):
+    if u == c:
+        return "It's a tie!"
+    elif u =="rock" and c =="scissors":
+        return "You win! Thanks for playing"
+    elif u =="paper" and c =="rock":
+        return "You win! Thanks for playing"
+    elif u =="scissors" and c =="paper":
+        return "You win! Thanks for playing"
     else:
-        print("Oops! Invalid choice. Please try again with rock, paper, or scissors.")
-        print("_______________________")
+        return "You lose! Better luck next time!"
 
-print(f"You chose {user_choice}.")
-print("_____________________")
+if __name__ == "__main__":
+    #  ONLY RUN THE CODE BELOW
+    # IF WE ARE RUNNING THIS SCRIPT FROM THE COMMAND LINE
+    # BUT NOT IF WE ARE TRYING TO JUST IMPORT SOME STUFF FROM THIS FILE
 
-#computer chooses randomly
-computer_choice=random.choice(valid_choices)
-print(f"The computer chose {computer_choice}.")
-print("_____________________")
+    #input validation loop
+    
 
-#Determine the winner
-if user_choice==computer_choice:
-    print("It's a tie!")
-elif user_choice=="rock" and computer_choice=="scissors":
-    print("You win! Thanks for playing")
-elif user_choice=="paper" and computer_choice=="rock":
-    print("You win! Thanks for playing")
-elif user_choice=="scissors" and computer_choice=="paper":
-    print("You win! Thanks for playing")
-else:
-    print("You lose! Better luck next time!")
-print("___________________")
-print("Goodbye and thanks for playing!")
+    
+    while True:
+        user_choice=input("Please choose rock, paper, or scissors: ").strip().lower()
+        if user_choice in valid_choices:
+            break
+        else:
+            print("Oops! Invalid choice. Please try again with rock, paper, or scissors.")
+            print("_______________________")
+
+    print(f"You chose {user_choice}.")
+    print("_____________________")
+
+    #computer chooses randomly
+    computer_choice=random.choice(valid_choices)
+    print(f"The computer chose {computer_choice}.")
+    print("_____________________")
+
+    #Determine the winner
+    # quick alias
+    u = user_choice
+    c = computer_choice
+ 
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
+    
+
+
+
