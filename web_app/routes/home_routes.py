697ef38c63493561d78e3e0772b145d8f01a5148
sth80-byte <sth80@georgetown.edu>
2025-12-06 10:03:35 -0500

    Pass variables to the page

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -33,4 +33,5 @@ def hello_world():
 
     message = f"Hello, {name}!"
     #return message
-    return render_template("hello.html", message=message)
+    y=20
+    return render_template("hello.html", message=message, x=5, y=y)
