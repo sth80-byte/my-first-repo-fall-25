e417a29645058bb017ff656d92a6c9e6ec871a92
sth80-byte <sth80@georgetown.edu>
2025-12-06 15:36:34 -0500

    Stocks dashbaord

--- app/stocks.py
+++ app/stocks.py
@@ -9,6 +9,9 @@ load_dotenv() # loads environment variables from the 'env' file
 
 ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
+
+def format_usd(price):
+    return f"${price:2f}"
 def fetch_stocks_csv(symbol="NFLX"):
     """Fetches stock data from the AlphaVantage API.
 
